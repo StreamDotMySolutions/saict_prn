0b6dc16f383487c41e6100c8aefda5990a549421
Azril Nazli Alias <azril.nazli@vireserve.com>
2023-06-17 10:28:30 +0800

    Update SideBar

--- frontend/src/Layouts/SideBar.js
+++ frontend/src/Layouts/SideBar.js
@@ -1,4 +1,4 @@
-import {NavLink, Link } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const SideBar = ( props ) => {
