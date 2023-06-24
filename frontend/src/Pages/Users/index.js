e4c33409c61dcba6f06d7ad33379fe01ae8f7d57
StreamDotMy Developer <streamdotmysolutions@gmail.com>
2023-06-24 23:56:00 +0800

    Pagination - refactor index.js

--- frontend/src/Pages/Users/index.js
+++ frontend/src/Pages/Users/index.js
@@ -3,45 +3,20 @@ import { Link } from "react-router-dom"
 import { useStore } from "../../Helpers/Store"
 import { BreadCrumb } from '../../Components/BreadCrumb'
 import { Pagination } from '../../Components/Pagination'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import axios from '../../Libs/axios'
 import Data from './data'
+import loadUsers from './loadUsers'
 
 const UsersIndex = () => {
-
     const [data, setData] = useState(null)
 
-    function loadUsers(){
-        // load users 
-        // api/users/index
-        console.log('load users from API server')
-
-        // submit as POST to API
-        axios({
-            url:  process.env.REACT_APP_BACKEND_URL + '/users/index',   
-            method: 'get',
-        })
-        .then( function(response){
-            //console.log(response)
-            setData(response.data.users)
-        })
-        .catch ( function(error){
-            console.log(error.response.data)
-        })
-    }
-
-    // 👇️ fetchData running once
-    useEffect(() => {
-        loadUsers()
-    },[])
-    //console.log(data)
+    useEffect(() => loadUsers({setData}),[])
 
     return (
         <>
             <BreadCrumb title='User Management'/>   
             <h1>User Management</h1>
             <hr />
-            { (data !== undefined && data !== null ) ?
+            { ( data !== undefined && data !== null ) ?
                 <> 
                 <Data />
                 <Pagination data={data} />
@@ -52,5 +27,4 @@ const UsersIndex = () => {
         </>
     )
 }
-
 export default  UsersIndex
---
+++ frontend/src/Pages/Users/loadUsers.js
@@ -0,0 +1,24 @@
+import axios from '../../Libs/axios'
+const loadUsers = (props) => {
+
+    // destructure the props passed to the component
+    //const { data, setData } = props;
+    
+    // load users 
+    // api/users/index
+    console.log('load users from API server')
+
+    // submit as POST to API
+    axios({
+        url:  process.env.REACT_APP_BACKEND_URL + '/users/index',   
+        method: 'get',
+    })
+    .then( function(response){
+        //console.log(response)
+        props.setData(response.data.users)
+    })
+    .catch ( function(error){
+        console.log(error.response.data)
+    })
+}
+export default loadUsers
