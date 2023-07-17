793f4b47f709684c17ab0b870ba26c3d4f88f72e
StreamDotMy Solutions <streamdotmysolutions@gmail.com>
2023-07-17 15:55:07 +0800

    sample navlink frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { NavLink} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav';
 
 
 const Home = () => {
