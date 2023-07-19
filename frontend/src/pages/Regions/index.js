a377cd28be89dc16b2557c4e5bcbf3cdd464171f
StreamDotMy Solutions <streamdotmysolutions@gmail.com>
2023-07-19 10:21:26 +0800

    UI/UX

--- frontend/src/pages/Regions/index.js
+++ frontend/src/pages/Regions/index.js
@@ -26,16 +26,22 @@ const Regions = () => {
     const candidateDataUrl = '/' + stateName + '/' + regionCode + '/' + regionName + '/'
     const listItems = candidates.map((candidate) =>
         
-    
-        <tr key={candidate.candidate_entry}>
-            <td>{candidate.candidate_entry}</td>
-            <td>
-            <Link to={candidateDataUrl + candidate.id + '/' + candidate.slug}>
-                {candidate.candidate_title?.toUpperCase()} {candidate.candidate_name?.toUpperCase()}
-            </Link>
-            </td>
-        </tr>
+    <li  key={candidate.candidate_entry} className="list-group-item">
+        {candidate.candidate_entry}. &nbsp;
+        <Link to={candidateDataUrl + candidate.id + '/' + candidate.slug}>
+            {candidate.candidate_title?.toUpperCase()} {candidate.candidate_name?.toUpperCase()}
+        </Link>
+    </li>
+        // <tr key={candidate.candidate_entry}>
+        //     <td>{candidate.candidate_entry}</td>
+        //     <td>
+        //     <Link to={candidateDataUrl + candidate.id + '/' + candidate.slug}>
+        //         {candidate.candidate_title?.toUpperCase()} {candidate.candidate_name?.toUpperCase()}
+        //     </Link>
+        //     </td>
+        // </tr>
    
+        
      
        
     );
@@ -43,7 +49,7 @@ const Regions = () => {
     return (
     <>
         <Container>
-            <small>
+      
             <Breadcrumb>
                 <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>
                     <FontAwesomeIcon icon="fas fa-home" />
@@ -55,12 +61,24 @@ const Regions = () => {
 
                 <Breadcrumb.Item active>{regionCode} {regionName.toUpperCase()}</Breadcrumb.Item>
             </Breadcrumb>
-            </small>
+      
 
-            <h2>{stateName.toUpperCase()} - {regionCode} {regionName.toUpperCase()}</h2>
+            
+            <div class="card">
+
+
+  <div class="card-body">
+    <h5 class="card-title">{stateName.toUpperCase()} - {regionCode} {regionName.toUpperCase()}</h5>
+    <p class="card-text">Penyandang ialah ...</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    {listItems}
+  </ul>
+
+</div>
 
         
-            { candidates.length > 0 ? 
+            {/* { candidates.length > 0 ? 
                 <table className='w-100 table table-striped'>
                     <thead>
                         <th style={{ width:'5px'}}>ID</th>
@@ -72,7 +90,7 @@ const Regions = () => {
                     </tbody>
                 </table>
                 : <p className='text-muted'>Belum ada pencalonan</p>
-            }
+            } */}
         </Container>
     </>
     )
