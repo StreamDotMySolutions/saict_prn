3d740811bbf080fac840afcefe7cf27df44eef8f
StreamDotMy Solutions <streamdotmysolutions@gmail.com>
2023-07-27 10:59:42 +0800

    Update .env | production or development

--- frontend/src/layouts/DefaultLayout.js
+++ frontend/src/layouts/DefaultLayout.js
@@ -8,26 +8,35 @@ import Row from 'react-bootstrap/Row';
 
 const DefaultLayout = () => {
 
+  const mode =  (process.env.REACT_APP_MODE)
+
   return (
     <>
-      {/* <TopNavBar/> 
-      <Container fluid className="p-1 mt-5">
-        <hr />
-        <Col lg={12}>
-          <Outlet />
-        </Col>
-        <Footer/>
-      </Container> */}
-      <Container>
-        <div className="d-flex justify-content-center">
-            <img src="img/maintainance.jpg" className="img-fluid" width="50%" title="maitainance" />
-        </div>
-        <Row>
-          <h2 className="text-center mt-3">Laman Web Dalam Penyelengaraan</h2>
-        </Row>
-      </Container>
+            { mode == 'production' ?
+                <>
+                  <TopNavBar/> 
+                  <Container fluid className="p-1 mt-5">
+                    <hr />
+                    <Col lg={12}>
+                      <Outlet />
+                    </Col>
+                    <Footer/>
+                  </Container> 
+                </>
+                  :
+                  <>
+                    <Container>
+                      <div className="d-flex justify-content-center">
+                          <img src="img/maintainance.jpg" className="img-fluid" width="50%" title="maitainance" />
+                      </div>
+                      <Row>
+                        <h2 className="text-center mt-3">Laman Web Dalam Penyelengaraan</h2>
+                      </Row>
+                    </Container>
+                  </>
+              }
     </>
-  );
+  ); // return 
 };
 
 export default DefaultLayout;
