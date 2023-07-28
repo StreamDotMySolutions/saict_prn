990392a77d2c85969e7afa5acce86fc8e8423823
epulhisham <syarifulhisham25@gmail.com>
2023-07-28 17:31:12 +0800

    hide arrow and garis

--- frontend/src/pages/Banner/BannerCarousel.js
+++ frontend/src/pages/Banner/BannerCarousel.js
@@ -1,16 +1,21 @@
+import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 
 const BannerCarousel = () => {
+
+  const imageSource = "img/WEB BANNER PRU15-01.png";
+  const totalImages = 1;
+
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="img/WEB BANNER PRU15-01.png"
-            alt="PRU DUN KE-15"
-          />
-        </Carousel.Item>
-      </Carousel>
+      <Carousel controls={totalImages > 1} indicators={totalImages > 1}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={imageSource}
+          alt="PRU DUN KE-15"
+        />
+      </Carousel.Item>
+    </Carousel>
     );
   };
   
