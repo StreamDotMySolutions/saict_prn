f33103a724f69ad8d2c0633f69dc2a6967ebc2a9
epulhisham <syarifulhisham25@gmail.com>
2023-08-01 17:00:56 +0800

    back to before

--- frontend/src/layouts/DefaultLayout.js
+++ frontend/src/layouts/DefaultLayout.js
@@ -24,26 +24,16 @@ const DefaultLayout = () => {
           <Footer/>
         </>
           :
-          // <>
-          //   <Container>
-          //     <div className="d-flex justify-content-center">
-          //         <img src="img/maintainance.jpg" className="img-fluid" width="50%" title="maitainance" />
-          //     </div>
-          //     <Row>
-          //       <h2 className="text-center mt-3">Laman Web Dalam Penyelengaraan</h2>
-          //     </Row>
-          //   </Container>
-          // </>
           <>
-          <TopNavBar/> 
-            <Container fluid className="p-1 mt-5">
-              <hr />
-              <Col lg={12}>
-                <Outlet />
-              </Col>
+            <Container>
+              <div className="d-flex justify-content-center">
+                  <img src="img/maintainance.jpg" className="img-fluid" width="50%" title="maitainance" />
+              </div>
+              <Row>
+                <h2 className="text-center mt-3">Laman Web Dalam Penyelengaraan</h2>
+              </Row>
             </Container>
-          <Footer/>
-        </>
+          </>
       }
     </>
   ); // return JSX
