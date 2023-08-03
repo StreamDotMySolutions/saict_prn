bf00fe75b486828cd4f8d3f690654d4ea8576b73
epulhisham <syarifulhisham25@gmail.com>
2023-08-03 11:59:41 +0800

    insert link to candidate name, region and state

--- frontend/src/pages/Dashboard/components/LatestCandidate.js
+++ frontend/src/pages/Dashboard/components/LatestCandidate.js
@@ -1,110 +1,106 @@
-import { React, useState , useEffect} from 'react'
-import axios from '../../../libs/axios'
-import { Link} from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { React, useState, useEffect } from 'react';
+import axios from '../../../libs/axios';
+import { Link, useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 /**
  * @return JSX
  */
 const LatestCandidate = () => {
-
-    const [latestCandidates,setLatestCandidates] = useState([])
-    const [isLoading,setIsLoading] = useState(false)
-
-
-    // useEffect(() => {
-    //     getLatestCandidates(setLatestCandidates)
-    //   }, [])
+    const { stateName } = useParams(); 
+    const [latestCandidates, setLatestCandidates] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-       
-        // loading for 1st time
-        getLatestCandidates(setLatestCandidates,setIsLoading)
-        
-        const intervalId = setInterval(() => {
-            getLatestCandidates(setLatestCandidates,setIsLoading)
-        }, 1000 * 5) // in milliseconds
+    // loading for 1st time
+    getLatestCandidates(stateName, setLatestCandidates, setIsLoading);
 
-        return () => clearInterval(intervalId)
-        
-    }, [])
-    
+    const intervalId = setInterval(() => {
+        getLatestCandidates(stateName, setLatestCandidates, setIsLoading);
+    }, 1000 * 5); // in milliseconds
 
-    const listCandidates = latestCandidates.map((item) =>
-        <tr key={item.id}>
-            <td className='text-center' >
-            {item.url ? (
-                <img src={item.url} className="rounded" style={{ width: '50px' }} />
-                ) : (
-                <img src="/img/no-image.png" className="rounded" style={{ width: '50px' }} />
-            )}
+    return () => clearInterval(intervalId);
+    }, [stateName]);
 
-            </td>
-            <td>    
-                <small>
-                    Data calon {''} 
-                    {item.candidate_title} {item.candidate_name} {''} 
-                    dari parti {item.party_name} {''} 
-                    di kawasan {item.region_code} {item.region_name}, {item.state_name} telah dikemaskini
-                    <br />
-                    <span className="text-muted">
-                        <FontAwesomeIcon icon="fas fa-clock" />
-                        <i>&nbsp;{item.when}</i>
-                    </span>
-                </small>
-            </td>
-        </tr>
-    );
+    const listCandidates = latestCandidates.map((item) => (
+    <tr key={item.id}>
+        <td className="text-center">
+        {item.url !== '' && <img src={item.url} className="rounded" style={{ width: '50px' }} />}
+        </td>
+        <td>
+            <small>
+                Data calon {''}
+                <Link
+                to={ '/' + item.state_name + '/' + item.region_code + '/' + item.region_name + '/' + item.id + '/' +  item.slug }
+                >
+                <b>{item.candidate_title} {item.candidate_name} {''}</b>
+                </Link>
+                dari parti {item.party_name} {''}
+                di kawasan {''}
+                <Link to={ '/' + item.state_name + '/' + item.region_code + '/' + item.region_name } >
+                <b>{item.region_code} {item.region_name}</b>, {''}
+                </Link>
+                <Link to={'/' + item.state_name}>
+                    <b>{item.state_name}</b>
+                </Link> {''}
+                telah dikemaskini
+                <br />
+                <span className="text-muted">
+                <FontAwesomeIcon icon="fas fa-clock" />
+                <i>&nbsp;{item.when}</i>
+                </span>
+            </small>
+        </td>
+    </tr>
+    ));
 
     return (
     <>
-        { latestCandidates.length > 0 ? 
-            <table className='w-100 table table-striped'>
-                <thead>
-                    <tr>
-                     
-                        <th colspan="2">
-                        <FontAwesomeIcon icon="fas fa-user" /> CALON 
-                        &nbsp; { isLoading ? <FontAwesomeIcon icon="fa-solid fa-spinner" /> : null }
-                        </th>
-                    </tr>
-                </thead>
+        {latestCandidates.length > 0 ? (
+        <table className="w-100 table table-striped">
+            <thead>
+                <tr>
+                    <th colSpan="2">
+                    <FontAwesomeIcon icon="fas fa-user" /> CALON &nbsp;{' '}
+                    {isLoading ? <FontAwesomeIcon icon="fa-solid fa-spinner" /> : null}
+                    </th>
+                </tr>
+            </thead>
 
-                <tbody>
-                    {listCandidates}
-                </tbody>
-            </table>
-            : <p className='text-muted'>Tiada data</p>
-         }
+            <tbody>
+                {listCandidates}
+            </tbody>
+        </table>
+        ) : (
+            <p className="text-muted">Tiada data</p>
+        )}
     </>
-    )
-}
-
+    );
+};
 
 /**
  * Get latest data PrnNomination via AXIOS
  * @param useState setLatestCandidates
  * @return useState setLatestCandidates
  */
-function getLatestCandidates(setLatestCandidates,setIsLoading){
-
-    setIsLoading(true)
+function getLatestCandidates(stateName, setLatestCandidates, setIsLoading) {
+    setIsLoading(true);
     axios({
-        url:  `${process.env.REACT_APP_BACKEND_URL}/prn-nominations/latest/candidates`,   
-        method: 'get',
-        headers: {
-            'Accept': 'application/json'
-        }
+    url: `${process.env.REACT_APP_BACKEND_URL}/prn-nominations/latest/candidates`,
+    method: 'get',
+    headers: {
+    Accept: 'application/json',
+    },
     })
-    .then( function(json){
-        //console.log(json.data.data)
-        setLatestCandidates(json.data.data)
-        setIsLoading(false)
+    .then(function (json) {
+      //console.log(json.data.data)
+        setLatestCandidates(json.data.data);
+        setIsLoading(false);
     })
-    .catch ( function(error){
-        //console.log(error.response.data)
-        setIsLoading(false)
-    })
+    .catch(function (error) {
+      //console.log(error.response.data)
+        setIsLoading(false);
+    });
 }
 
-export default LatestCandidate
+export default LatestCandidate;
