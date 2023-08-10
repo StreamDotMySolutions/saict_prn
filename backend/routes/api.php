dd95de91e889a02bd841f7e1150699207832bb04
StreamDotMy Solutions <streamdotmysolutions@gmail.com>
2023-08-10 10:27:56 +0800

    add route /

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'Laravel API powered by Octane / Swoole';
+});
+
 // auth
 require __DIR__.'/auth.php';
 
